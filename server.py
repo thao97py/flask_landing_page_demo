a1972f312f4929fd67d3dd2234c68ff8a534d8ea
Thao Truong <phuongthao97py@gmail.com>
2018-05-13 18:50:36 -0700

    mades changes on the server.py file

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 @app.route('/')
 def greeting():
     return render_template('index.html', name = 'World')
+
+@app.route('/dojos/new',methods=['GET','POST'])
+def dojos():
+    name = request.form["name"]
+    email = request.form["email"]
+    return redirect('/')
+
 @app.route('/ninjas')
 def info_ninja():
     return render_template('ninjas.html', name='Ninja Thao')
-@app.route('/dojos/new', methods=['POST'])
-def dojos():
-    name = request.form['name']
-    email = request.form['email']
-    return redirect('/')
+
 app.run(debug=True)
